ef66d22fdca67cc0533911275dbd9296ed632377
Munkhgerelt <munkhgerelt2005@gmail.com>
2025-03-21 04:20:41 +0800

    Fixed the Inheritance and Delegation classes.

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,27 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+ public class InheritanceSortedIntList extends SortedIntList {
+    private int totalAdded = 0;
 
+    @Override
+    public boolean add(int num) {
+        totalAdded++;
+        return super.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        int countBefore = totalAdded;
+        
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
+        
+        return totalAdded > countBefore;
+    }    
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
